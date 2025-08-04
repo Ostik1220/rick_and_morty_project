167204762c2d80d855636c612cc971836ed91cde
Ostik <117195748+Ostik1220@users.noreply.github.com>
2025-08-04 16:16:08 +0300

    save

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-const hi = 'Hello, World!';
+import './js/home.js';
+import './js/characters.js';
+import './js/episodes.js';
+import './js/footer.js';
+import './js/header.js';
+import './js/pop-up.js';
