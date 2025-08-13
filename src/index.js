833448ef34af477966b2d7f278b8183b400c3034
Ostik <117195748+Ostik1220@users.noreply.github.com>
2025-08-13 11:32:24 +0300

    done get api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './js/home.js';
 import './js/characters.js';
-import './js/episodes.js';
+import './js/episodes.js'
 import './js/footer.js';
 import './js/header.js';
 import './js/pop-up.js';
+import './js/api-requests/getListsApi.js';
