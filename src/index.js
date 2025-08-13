b6e0899fe85b6fb5c1b238ec7bb89a50522a4b15
Ostik <117195748+Ostik1220@users.noreply.github.com>
2025-08-13 11:34:19 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './js/home.js';
 import './js/characters.js';
-import './js/episodes.js'
+import './js/episodes.js';
 import './js/footer.js';
 import './js/header.js';
 import './js/pop-up.js';
