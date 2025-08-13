4a2a7419e1d2549332f553b95492985c47df8d61
Ostik <117195748+Ostik1220@users.noreply.github.com>
2025-08-14 02:26:25 +0300

    save

--- src/js/header.js
+++ src/js/header.js
@@ -4,11 +4,41 @@ getCharacterList(3).then(data => {
   console.log(data);
 })
 
-const inputHeader = document.querySelector('#header__input');
-console.log(inputHeader);
-inputHeader.addEventListener('input', (e) => {
- console.log(e.target.value);
+const headerForm = document.querySelector('.header__form');
+
+headerForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const searchValue = document
+    .querySelector('.header__search')
+    .value.trim()
+    .toLowerCase();
+
+  try {
+    const firstPage = await getCharacterList(1);
+    const totalPages = firstPage.info.pages;
+    const promises = [];
+    for (let i = 2; i <= totalPages; i++) {
+      promises.push(getCharacterList(i));
+    }
+    const otherPages = await Promise.all(promises);
+    const allCharacters = [
+      ...firstPage.results,
+      ...otherPages.flatMap(page => page.results)
+    ];
+    const filteredCharacters = allCharacters.filter(char =>
+      char.name.toLowerCase().includes(searchValue)
+    );
+
+    console.log(filteredCharacters);
+
+  } catch (error) {
+  
+  }
+
+  document.querySelector('.header__search').value = '';
 });
+
 
 const scrollButton = document.querySelector('scroll-down');
 
