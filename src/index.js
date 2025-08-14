f7486f13d0e9d07f8fc08eea176df3bd108d30a4
Kondrov220 <121194027+Kondrov220@users.noreply.github.com>
2025-08-14 16:45:16 +0300

    fix-deploy

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-// import './js/home.js';
-import './js/characters.js';
-import './js/char-modal.js';
-// import './js/episodes.js';
-import './js/footer.js';
 import './js/header.js';
-import './js/pop-up.js';
-import './js/api-requests/getListsApi.js';
+import './js/home.js';
+// import './js/characters.js';
+// import './js/char-modal.js';
+// import './js/episodes.js';
+// import './js/pop-up.js';
+// import './js/api-requests/getListsApi.js';
+import './js/footer.js';
